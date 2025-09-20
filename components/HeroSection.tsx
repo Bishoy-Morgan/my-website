004906831b0855940296d50b19be0e3e5dfff0f1
Bishoy Morgan <bishoy.morgan95@gmail.com>
2025-09-20 08:02:16 +0300

    opt.Herosection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -16,30 +16,34 @@ const HeroSection = () => {
         <main className='relative w-full h-dvh min-h-screen max-h-[1080px] flex justify-center overflow-x-hidden'>
             <section className='w-full md:w-4/5 h-full flex justify-center md:items-center md:justify-center '>
                 <motion.div
-                    className={`w-95 h-[95%] md:h-[90%] md:w-1/2 flex flex-col items-center justify-end text-center `}
-                    initial={{ opacity: 0, x: -20 }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 1, ease: "easeOut" }}
-                    viewport={{ once: true }}
+                className="w-95 h-[95%] md:h-4/5 md:w-1/2 md:mt-[5%] flex flex-col justify-between text-center"
+                initial={{ opacity: 0, x: -20 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+                viewport={{ once: true }}
                 >
-                    <h1 className='!px-4 md:px-0'>
+                <div className="flex flex-col items-center justify-center flex-grow">
+                    <h1 className="px-4 md:px-0">
                         {t("title")}
                     </h1>
-                    <div className="w-4/5 flex justify-start mb-24 md:mt-6 md:mb-20">
-                        <p className="main-p text-paleWhite py-10 px-2">
-                            {t("paragraphA")}
-                        </p>
-                    </div>
+                    <p className="main-p text-paleWhite py-[7%] px-2 md:px-12">
+                        {t("paragraphA")}
+                    </p>
+                </div>
+                <div className="mb-4">
                     <a
                     href="https://cal.com/bishoy-morgan"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="flex justify-center"
                     >
                         <Button bgColor="#ff220e">
                             {t("button")}
                         </Button>
                     </a>
+                </div>
                 </motion.div>
+
                 <motion.div
                     className='black-gradient absolute top-0 left-0 w-full md:px-1 h-full -z-10 md:-z-50 opacity-40 md:opacity-100'
                     onViewportEnter={() => setShowBeams(true)}
