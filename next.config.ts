025e3b79d3a0ec743aa74969cfa0f462b23b9325
Bishoy Morgan <bishoy.morgan95@gmail.com>
2025-09-27 13:15:36 +0300

    opti-intl

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+    optimizeCss: true,
+    optimizePackageImports: [
+        'lucide-react', 
+        'date-fns',       
+        'lodash',         
+        'react-icons'     
+    ]
+    },
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
+    reactStrictMode: true,                 
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
